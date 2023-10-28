a8769e712881ad8c2753d27cb48db051e57b3357
Sebastian Słoma <sebastiansloma@gmail.com>
2023-10-28 10:52:34 +0200

    prepare project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <h1 className='text-bold text-teal-500'>hello world</h1>
+    <h1 className='text-bold text-teal-500 text-2xl'>hello world</h1>
   )
 }
