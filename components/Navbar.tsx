09b0ec7534391fc73d0080c61886e5b563da22b1
Sebastian Słoma <sebastiansloma@gmail.com>
2023-11-03 19:45:24 +0100

    add logo to nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
-  return (
-    <nav className='border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5'>
-<Link href="/">
-    Home
-</Link>
-    </nav>
-  )
-}
+	return (
+		<nav className='flexBetween max-container padding-container relative z-30 py-5'>
+			<Link href='/'>
+				<Image src={'/hilink-logo.svg'} alt={'logo'} width={74} height={29} />
 
-export default Navbar
+        <ul className='hidden h-full gap-12 lg:flex'>
+
+        </ul>
+			</Link>
+		</nav>
+	);
+};
+
+export default Navbar;
