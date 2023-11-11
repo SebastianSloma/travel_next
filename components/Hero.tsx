18116a8bdfa7c25e711ff93fdfcbc2845fb6f361
Sebastian Słoma <sebastiansloma@gmail.com>
2023-11-11 16:25:58 +0100

    add hero map

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+      <div className='hero-map'/>
+    </section>
+	);
+};
 
-export default Hero
+export default Hero;
