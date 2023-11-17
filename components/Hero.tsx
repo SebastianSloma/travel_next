b6a6433719cece9ce90ae6c818f5fdf5664f771a
Sebastian Słoma <sebastiansloma@gmail.com>
2023-11-17 10:02:11 +0100

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,8 @@ const Hero = () => {
 	  {/* LEFT */}
 		<div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
 			<Image src='/camp.svg' alt='camp' width={50} height={50} className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'/>
-			<h1>Camp Arena</h1>
+			<h1 className='bold-52 lg:bold-88'>Camp Arena</h1>
+			<p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>lalala</p>
 		</div>
 
     </section>
