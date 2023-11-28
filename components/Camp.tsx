e442a1b981c95898dcc4c67d5f7ad3580042bda5
Sebastian Słoma <sebastiansloma@gmail.com>
2023-11-28 08:27:19 +0100

    .

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CampSite = () =>{
   return(
     <div>
-      
+      Camp Site 1
     </div>
   )
 }
@@ -13,7 +13,7 @@ const Camp = () => {
   return (
     <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
       <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400] xl:h-[640px]'>
-
+        <CampSite/>
       </div>
 
     </section>
