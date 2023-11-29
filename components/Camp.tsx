967a0a1ab23450b259a061ce2d6e2febc97a57da
Sebastian Słoma <sebastiansloma@gmail.com>
2023-11-29 08:18:02 +0100

    props

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const CampSite = () =>{
-  return(
-    <div>
-      Camp Site 1
-    </div>
-  )
+interface CampProps {
+	backgroundImage: string;
+	title: string;
+	subtitle: string;
+	peopleJoined: string;
 }
 
+const CampSite = ({
+	backgroundImage,
+	title,
+	subtitle,
+	peopleJoined,
+}: CampProps) => {
+	return <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl `}>Camp Site 1</div>;
+};
 
 const Camp = () => {
-  return (
-    <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
-      <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400] xl:h-[640px]'>
-        <CampSite/>
-      </div>
+	return (
+		<section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+			<div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400] xl:h-[640px]'>
+				<CampSite
+					backgroundImage='bg-bg-img-1'
+					title='Tents Camp'
+					subtitle='Trigon, Hagen'
+					peopleJoined='50+ Joined'
+				/>
+			</div>
+		</section>
+	);
+};
 
-    </section>
-  )
-}
-
-export default Camp
+export default Camp;
