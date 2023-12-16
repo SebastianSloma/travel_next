76705906156b3606ebaa39e38a64c64674d45504
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-16 23:44:36 +0100

    created guide section part 1

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Guide = () => {
-  return (
-    <div>Guide</div>
-  )
-}
+	return (
+		<section className='flexCenter flex-col'>
+			<div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+				<Image src='/camp.svg' alt='camp' width={50} height={50} />
+				<p>We Are Here For You</p>
+			</div>
+		</section>
+	);
+};
 
-export default Guide
+export default Guide;
