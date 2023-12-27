5f4d5efd2fc70ffc95e6c507bc7f2cbdc8d70b20
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-27 13:48:23 +0100

    finish getapp section

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import Image from 'next/image';
 
 const GetApp = () => {
 	return (
@@ -12,23 +13,25 @@ const GetApp = () => {
 					<p className='regular-16 text-gray-10'>
 						Available on iOS and Android
 					</p>
-          <div className='flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row'>
-            <Button
-            type='button'
-            title='App Store'
-            icon='/apple.svg'
-            variant='btn_white'
-            full
-            />
-            <Button
-            type='button'
-            title='Play Store'
-            icon='/android.svg'
-            variant='btn_dark_green_outline'
-            full
-            />
-
-          </div>
+					<div className='flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row'>
+						<Button
+							type='button'
+							title='App Store'
+							icon='/apple.svg'
+							variant='btn_white'
+							full
+						/>
+						<Button
+							type='button'
+							title='Play Store'
+							icon='/android.svg'
+							variant='btn_dark_green_outline'
+							full
+						/>
+					</div>
+				</div>
+				<div className='flex flex-1 items-center justify-end'>
+					<Image src='/phones.png' alt='phones' width={550} height={870} />
 				</div>
 			</div>
 		</section>
