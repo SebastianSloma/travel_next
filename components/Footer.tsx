3381b500d86739fdd712047028aa2e3334fe9f4a
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-28 18:39:39 +0100

    prepare footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer className='flexCenter mb-24'>
+			<div className='padding-container max-container flex w-full flex-col gap-14'>
+				<div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+					<Link href='/' className='mb-10'>
+						<Image src={'/logo.png'} alt={'logo'} width={74} height={29} />
+					</Link>
+				</div>
+			</div>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
