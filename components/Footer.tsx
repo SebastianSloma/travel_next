fb80ced30834a39d59a34dabbb1918c101444bd1
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-29 21:42:36 +0100

    added link map

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import { FOOTER_LINKS } from '@/constants';
 
 const Footer = () => {
 	return (
@@ -10,12 +11,36 @@ const Footer = () => {
 					<Link href='/' className='mb-10'>
 						<Image src={'/logo.png'} alt={'logo'} width={74} height={29} />
 					</Link>
-          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-
-          </div>
+					<div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+						{FOOTER_LINKS.map(columns => (
+							<FooterColumn title={columns.title}>
+								<ul className='regular-14 flex flex-col gap-4 text-gray-30'>
+									{columns.links.map((link)=>(
+										<Link href='/' key={link}>
+										{link}
+										</Link>
+									))}
+								</ul>
+								</FooterColumn>
+						))}
+					</div>
 				</div>
 			</div>
 		</footer>
+	);
+};
+
+type FooterColumnProps = {
+	title: string;
+	children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+	return (
+		<div className='flex flex-col gap-5'>
+			<h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+			{children}
+		</div>
 	);
 };
 
